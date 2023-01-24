b08e944cf24b64a67a7e46c281ddf63e628a3ad0
Andrew Hinton <ahinton@clearblade.com>
2023-01-24 10:12:30 -0600

    fixed error

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package main
+package iot
 
 import (
 	"context"
@@ -12,12 +12,16 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
-	device := Device{
-		Blocked:  true,
-		LogLevel: "NONE",
+	// device := Device{
+	// 	Blocked:  true,
+	// 	LogLevel: "NONE",
+	// }
+	getDevice := NewProjectsLocationsRegistriesDevicesService(service)
+	call := getDevice.Get(fmt.Sprintf("projects/%s/locations/%s/registries/%s/devices/%s", "iot-core-test-363819", "us-central1", "andrewtest3", "andrewtest"))
+	resp, err := call.Do()
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(resp.NumId)
 	}
-	createDevice := NewProjectsLocationsRegistriesDevicesService(service)
-	call := createDevice.Patch(fmt.Sprintf("projects/%s/locations/%s/registries/%s/devices/%s", "iot-core-test-363819", "us-central1", "andrewtest2", "go-sdk-patch-test"), &device)
-	call.UpdateMask("blocked,logLevel,metadata")
-	call.Do()
 }
